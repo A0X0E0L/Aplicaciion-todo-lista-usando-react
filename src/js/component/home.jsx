11ca287c3ad51cbf30e1737c4b5261373d0d2055
A0X0E0L <ax13xxi@gmail.com>
2023-01-12 03:38:52 +0000

    Lista de taereas con react finalizada al fin

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,18 +1,76 @@
 import React, {useState} from "react";
-import Card from "./Card.jsx";
-
-
-
 
 const Home = () => {
-  
-   
-	return (
-   <>
-  <Card/>
+    const [input, setInput] = useState("");
+    const [todos, setTodos] = useState([]);
 
-	</>
-	);
+
+    function handleInput(e) {
+        setInput(e.target.value);
+    }
+
+    const handleClick = () => {
+        if (input.length ===0 ) {
+            alert("Debe ingresar un valor")
+        } else {
+            setTodos(todos.concat([input]));
+            setInput("");
+        }
+    }
+
+    const clickBorrar = () => {
+        setTodos([])
+    }
+
+
+    return (
+        <div className="container">
+
+            <div className="m-auto tamañoDiv padding-superior ">
+                <div className="p-3 color-de-fondo">
+                <h5 className="pb-3 text-light ">
+                     Ingresar tareas a realizar porfa:</h5>
+                <div className="input-group mb-3">
+                    <button onClick={handleClick}
+                        className="btn-group btn-group-lg"
+                        type="button"
+                        id="button">Ingresar Tarea</button>
+                    <input onChange={handleInput}
+                        type="text"
+                        className="form-control"
+                        placeholder=""
+                        value={input}
+                        aria-label="Example text with button addon"
+                        aria-describedby=""/>
+                </div>
+                <div id="contenedorTodos" className="bg-secondary text-light">
+                  
+                    {
+                    todos.map((item, index) => (
+                        <div className="row d-flex p-2">
+							<div className="col-6">
+                                <h5 class>{item}</h5>
+                            </div>
+							<div className="col-6 text-end">
+                           
+							</div>
+							</div>                        
+                    ))
+                } </div>
+                <div id="contadorTodos">
+                    <p className="text-light">{
+                        todos.length
+                    }
+                        &nbsp;Tareas faltantes</p>
+                </div>
+                <button onClick={clickBorrar}
+                    className="btn-group btn-group-lg"
+                    type="button"
+                    id="button-addon1">Eliminar</button>
+            </div>
+        </div>
+        </div>
+    );
 };
 
 export default Home;
--- src/js/index.js
+++ src/js/index.js
@@ -2,13 +2,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-// include your styles into the webpack bundle
-import "../styles/index.css";
-import Card from "./component/Card.jsx";
-import Home from "./component/home.jsx";
 
 //import your own components
-
+import Home from "./component/home.jsx";
+import "../styles/index.css";
 
 //render your react application
-ReactDOM.render(<Home/> , document.querySelector("#app"));
+ReactDOM.render(<Home />, document.querySelector("#app"));
